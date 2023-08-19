d1f85c604171708d6e90b069ecc5670ca6f9c8cc
Ian Loron de Almeida <i198933@dac.unicamp.br>
2023-08-19 16:36:56 -0300

    code organization

--- backend/questionaire.sql
+++
@@ -1,9 +0,0 @@
--- to run this script, run:
--- mysql -P 3306 -u <user> -p < ./questionaire.sql 
-
-CREATE DATABASE IF NOT EXISTS questionaire;
-USE questionaire;
-CREATE TABLE stakeholders (
-    sector INT NOT NULL,
-    suggestion VARCHAR(1000) NOT NULL
-);

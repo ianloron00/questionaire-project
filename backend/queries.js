369b004060830a1f426ca4f38c81565549fc8804
Ian Loron de Almeida <i198933@dac.unicamp.br>
2023-08-20 20:49:40 -0300

    POST implementation

--- backend/queries.js
+++ backend/queries.js
@@ -1,14 +1,27 @@
 const connection = require('./db_manager/connection')
+const database_name = 'sakila.actor'
 
 const getAllActors = async () => {
-    const [query] = await connection.execute('SELECT * FROM sakila.actor')
+    const [query] = await connection.execute(`SELECT * FROM ${database_name}`)
     return query
 }
 
 const getActorsById = async (id) => {
-    const [query] = await connection.execute(`SELECT * FROM sakila.actor WHERE
+    const [query] = await connection.execute(`SELECT * FROM ${database_name} WHERE
     actor_id = ?`, [id])
     return query
 }
 
-module.exports = { getAllActors, getActorsById }
+const createActor = async (first_name, last_name) => {
+    const [query] = await connection.execute(`INSERT INTO 
+    ${database_name} (first_name, last_name) 
+    VALUES (?, ?)`, [first_name, last_name])
+    const new_item = await getActorsById(query.insertId)
+    return new_item
+}
+
+module.exports = { 
+    getAllActors, 
+    getActorsById, 
+    createActor 
+}
--- backend/routes/actors.js
+++ backend/routes/actors.js
@@ -7,11 +7,19 @@ router.get('/', async(req, res) => {
     return res.status(200).json(query)
 })
 
+// Em req.params estará os parâmetros passados na URL
+// Neste caso, o parâmetro 'id'
 router.get('/:id', async(req, res) => {
     const { id } = req.params
     const query = await queries.getActorsById(id)
     if (!query.length)
         return res.status(400).json({ message: 'actor not found' })
+    return res.status(200).json(query)
+})
+
+router.post('/', async (req, res) => {
+    const { first_name, last_name } = req.body
+    const query = await queries.createActor(first_name, last_name)
     return res.status(200).json(query)
 })
 

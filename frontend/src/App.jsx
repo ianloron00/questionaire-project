b684080a56431be24ae344cb3c1e6fe323c7b1ce
Ian Loron de Almeida <i198933@dac.unicamp.br>
2023-08-23 20:15:49 -0300

    Aligned forms in the center of the page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Forms from './Forms'
 import './App.css'
 
 function App() {
@@ -7,24 +8,7 @@ function App() {
   return (
     <div className="App">
       <h1>Questionário</h1>
-      <form className="form">
-        <select>
-            <option value="undefined">escolha um...</option>
-            <option value="frontend">Front-end</option>
-            <option value="backend">Back-end</option>
-            <option value="qa">QA</option>
-            <option value="devops">DevOps</option>
-            <option value="outro">Outro</option>
-        </select>
-        <textarea 
-            placeholder="Dê sua sugestão"
-            id="suggestions box"
-            name="suggestions box"
-            rows="15"
-            cols="75">
-        </textarea>
-        <input type="submit" value="Submeter" />
-      </form>
+      <Forms />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           número de colaboradores é {count}
---
+++ frontend/src/Forms.jsx
@@ -0,0 +1,28 @@
+import './Forms.css'
+
+function Forms() {
+    return (
+        <form className="form">
+            <select>
+                <option value="undefined">escolha um...</option>
+                <option value="frontend">Front-end</option>
+                <option value="backend">Back-end</option>
+                <option value="qa">QA</option>
+                <option value="devops">DevOps</option>
+                <option value="outro">Outro</option>
+            </select>
+            <textarea 
+                placeholder="Dê sua sugestão"
+                id="suggestions box"
+                name="suggestions box"
+                rows="15"
+                cols="75"
+                className="textarea">
+            </textarea>
+            <input type="submit" value="Submeter" />
+        </form>
+    )
+}
+
+export default Forms
+
